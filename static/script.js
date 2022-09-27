74c4cd49e35b65c6ae70db0870c251a53cb790cb
Dylan Keller <dekeller2024@loyolablakefield.org>
2022-09-27 11:44:30 -0400

    adding functionality to meter switches

--- static/script.js
+++ static/script.js
@@ -1,7 +1,10 @@
-function validate(){
+function meters(){
     var check = document.getElementById('switch');
 
     if (check.checked){
         alert("You have checked the box")
-}
+    }
+    else {
+        alert("Box not checked")
+    }
 }
