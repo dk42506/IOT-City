68fb23ec61bfb72d8c629a5ffb8cc9d2b8a52a1c
Dylan Keller <dekeller2024@loyolablakefield.org>
2022-10-04 08:44:25 -0400

    working on billboard

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
+
+f = open('templates/index.html', 'w')
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("index.html")
-
-
-#def billboard():
-
+    if request.method == "POST":
+        user = request.form["nm"]
+        return user
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
