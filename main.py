36e95ca5aec52a5f60b46c8611c1391aec56c9d5
Dylan Keller <dekeller2024@loyolablakefield.org>
2022-10-04 16:09:34 -0400

    trying to add ssh functionality

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import paramiko
+import sys
 from flask import Flask, render_template, request
 
-f = open('templates/index.html', 'w')
+def billboard(processed_text):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.connect('10.4.2.41', username= 'icsserver', password= 'password')
+    ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command('python lcdScreen ' + str(processed_text))
+    sys.exit()
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return user
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/", methods=['POST'])
+def homePost():
+    text = request.form['nm']
+    processed_text = text.upper()
+    billboard(processed_text)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
