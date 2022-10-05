42a61ce732cbbefea3f55569e7b943f21807e81d
Dylan Keller <dekeller2024@loyolablakefield.org>
2022-10-05 10:30:15 -0400

    adding old text

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import paramiko
-import sys
 from flask import Flask, render_template, request
-
-def billboard(processed_text):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.connect('10.4.2.41', username= 'icsserver', password= 'password')
-    ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command('python lcdScreen ' + str(processed_text))
-    sys.exit()
 
 app = Flask(__name__)
 
@@ -19,8 +10,7 @@ def home():
 def homePost():
     text = request.form['nm']
     processed_text = text.upper()
-    billboard(processed_text)
-    return render_template("index.html")
+    return render_template("index.html", variable=processed_text)
 
 if __name__ == "__main__":
     app.run()
