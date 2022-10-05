2b92505cdd90cb56d2ed93a3f0020ff4ff550ec1
Dylan Keller <dekeller2024@loyolablakefield.org>
2022-10-05 19:11:10 -0400

    bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from concurrent.futures import ProcessPoolExecutor, process
-from pexpect import pxssh
+#from pexpect import pxssh
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -12,9 +11,9 @@ def home():
 def homePost():
     text = request.form['nm']
     processed_text = text.upper()
-    ssh = pxssh.pxssh()
-    ssh.login("192.168.1.164", "dk42506", "DylanK6205")
-    ssh.sendline("touch " + str(processed_text) + ".txt")
+    #ssh = pxssh.pxssh()
+    #ssh.login("192.168.1.164", "dk42506", "DylanK6205")
+    #ssh.sendline("touch " + str(processed_text) + ".txt")
     return render_template("index.html", variable=processed_text)
 
 if __name__ == "__main__":
