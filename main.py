eb6064410e748bcfa396d9bc153e195e302e3e36
Dylan Keller <dekeller2024@loyolablakefield.org>
2022-10-05 21:44:06 -0400

    done but no map

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ def homePost():
     processed_text = text.upper()
     ssh = pxssh.pxssh()
     #("host", "user", "password")
-    ssh.login("10.4.2.5", "icsserver", "password")
-    # get rid of this command and add ("python3 lcdscreen.py " + str(processed_text)
+    ssh.login("10.4.2.41", "icsserver", "password")
     ssh.sendline("touch " + str(processed_text) + ".txt")
     return render_template("index.html", variable=processed_text)
 
